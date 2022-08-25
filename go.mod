3c1d074a207ffdcf2e3f1b969826c3a69d1e026e
Ning-Qing <2582820807@qq.com>
2022-08-25 09:53:54 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ning-Qing/proxy-go-get
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"path"
+	"strings"
+)
+
+const tpl = `
+<meta name="go-import" content="%s git ssh://git@git.vonechain.com/%s">
+`
+
+var addr = flag.String("addr", "127.0.0.1:9090", "listening address")
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", handleGoGet)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServer: ", err)
+	}
+}
+
+func handleGoGet(w http.ResponseWriter, r *http.Request) {
+	pkgPath := packagePath(r.URL.Path)
+	host := r.Host
+	root := fmt.Sprintf("%s/%s", host, pkgPath)
+	_, _ = fmt.Fprintf(w, tpl, root, pkgPath)
+}
+
+func packagePath(p string) string {
+	s := strings.Split(p, "/")
+	items := make([]string, 0, 2)
+	for _, n := range s {
+		if n == "" {
+			continue
+		}
+		items = append(items, n)
+		if len(items) == 2 {
+			break
+		}
+	}
+	return path.Join(items...)
+}
